89db993d6df4c877e28cc5194c47f2e3b4d336f8
humanshield85 <rachidboudjelida@gmail.com>
2023-09-16 17:47:48 +0100

    fix

--- page1/script.js
+++ page1/script.js
@@ -1,3 +1,23 @@
-console.log('Hello world')
+function isNegative(x) {
+    if(x < 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-alert('Hello world');
+function absoluteValue(x) {
+    // should return |x|
+    // if x is negative we return x * -1
+    if(isNegative(x)){
+        return x * -1;
+    } else {
+        return x
+    }
+}
+
+var number = 8;
+
+var absolute = absoluteValue(number);
+
+console.log(`The absolute value of ${number} is ${absolute}`)
